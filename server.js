e0e36f1825837584e6d9c68032bf373514988a5f
Vitchan_JIN <gohst1217@gmail.com>
2022-01-11 21:45:41 +0900

    server

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require('fs');
-// const res = require("express/lib/response");
-const app = express();
-const router = express.Router();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const path = require('path');
-// multer : image upload
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'recruitImg')
-  },
-  filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-})
-
-const upload = multer({storage : storage});
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
@@ -32,66 +11,9 @@ app.get("/recruit-form", (req, res) => {
   res.sendFile(__dirname + "/public/recruit-form.html");
 });
 
-app.post("/upload", upload.single('newFile') ,(req, res) => {
-  const file = req.file;
-  // res.send("Image Uploaded");
-  console.log(file) // object를 리턴함
-});
+
 
 module.exports = router;
-
-
-// const nodemailer = require('nodemailer');
-
-// app.use(express.json());
-
-// app.post('/', (req, res) => {
-
-//   const output = `
-//   <p>새로운 지원 메일이 도착했습니다.</p>
-//   <h3>지원자 정보</h3>
-//   <ul style="list-style: none;">
-//     <li style="list-style: none;">Name: ${req.body.name}</li>
-//     <li style="list-style: none;">phone: ${req.body.phone}</li>
-//     <li style="list-style: none;">email: ${req.body.email}</li>
-//     <li style="list-style: none;">apply: ${req.body.apply}</li>
-//     <li style="list-style: none;">file: ${req.body.files}</li>
-//   </ul>
-//   <h3>상세 내용</h3>
-//   <p>${req.body.content}</p>
-//   `
-
-//   console.log(req.body);
-
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: 'yhklilly0305@gmail.com',
-//       pass: 'dbghkrud0305'
-//     }
-//   })
-
-//   const mailOptions = {
-//     from: req.body.email,
-//     to: 'yhklilly0305@gmail.com',
-//     subject: '지원 메일 from ' + req.body.name,
-//     html: output
-//   }
-
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.log(error)
-//       res.send('error')
-//     } else {
-//       console.log('Email sent: ' + info.response)
-//       res.send('success')
-//     }
-
-//   })
-// })
-
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
