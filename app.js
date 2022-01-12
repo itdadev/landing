babbc6e315086b3c9dcd5f319982f0ab9e48548b
unknown <lilly@itdadev.com>
2022-01-12 15:14:13 +0900

    [22-01-12 15:14] - [x] recruit-form 기능 구현 완료 - [ ] 파일 선택 버튼 custom 필요 - [ ] contact form 기능 구현 필요

---
+++ app.js
@@ -0,0 +1,121 @@
+const nodemailer = require('nodemailer');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// app.get("/recruit-form.html", (req, res) => {
+//   res.sendFile(__dirname + "/public/recruit-form.html");
+// });
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
+
+var name;
+var phone;
+var email;
+var content;
+var path;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./attachments")
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
+  }
+});
+
+const upload = multer({storage: storage, limits: { fileSize: 10 * 1024 * 1024 }}).single('image');
+
+
+app.post("/requst-form.html", (req, res) => {
+  // execute this middleware to upload the image
+  upload(req, res, function(err){
+    if(err){
+      console.log(err)
+      return res.end("Please check your file size (maximum: 100MB)")
+    }else{
+      name = req.body.name
+      phone = req.body.phone
+      email = req.body.email
+      content = req.body.content
+
+      path = req.file.path
+
+      // console.log(name)
+      // console.log(phone)
+      // console.log(email)
+      // console.log(content)
+      // console.log(path)
+
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'yhklilly0305@gmail.com',
+          pass: 'dbghkrud0305'
+        }
+      })
+      const output = `
+        <p>새로운 지원 메일이 도착했습니다.</p>
+        <h3>지원자 정보</h3>
+        <ul style="list-style: none;">
+          <li style="list-style: none;">Name: ${req.body.name}</li>
+          <li style="list-style: none;">phone: ${req.body.phone}</li>
+          <li style="list-style: none;">email: ${req.body.email}</li>
+          <li style="list-style: none;">apply: ${req.body.apply}</li>
+        </ul>
+        <h3>상세 내용</h3>
+        <p>${req.body.content}</p>
+        `
+      
+      var mailOptions = {
+        from: req.body.email,
+        to: 'yhklilly0305@gmail.com',
+        subject: '지원 메일 from ' + req.body.name,
+        html: output,
+        attachments: [
+          {
+            path: path
+          }
+        ]
+      }
+
+      transporter.sendMail(mailOptions, function(err,info){
+        if(err){
+          console.log(err)
+        }else{
+
+          fs.unlink(path, function(err){
+            if(err){
+              return res.end(err)
+            }else{
+              res.send("<script>alert('지원해주셔서 감사합니다!');location.href='/recruit.html';</script>");
+            }
+          })
+        }
+      })
+    }
+  })
+
+});
