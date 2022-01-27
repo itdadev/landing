74866d1cae91b321c0830baa962b4459e727dd0b
lilly0305 <yhklilly0305@gmail.com>
2022-01-27 17:17:18 +0900

    common.js 헤더 공통 스타일링 수정

--- public/js/common.js
+++ public/js/common.js
@@ -1,14 +1,70 @@
-$(window).scroll(function () {
-	var scroll = $(document).scrollTop();
+$(window).resize(function () {
+  if ($(window).width() < 1024) {
 
-  if(scroll == 0) {
-    $('.mobileHeader').stop().animate({opacity: '1'},200).css({display: 'block'});
-  }else{
-    $('.mobileHeader').stop().animate({opacity: '0'},200).css({display: 'none'});
+    $(window).scroll(function () {
+      var scroll = $(document).scrollTop();
+
+      if (scroll == 0) {
+        $('.mobileHeader').stop().animate({
+          opacity: '1'
+        }, 200).css({
+          display: 'block'
+        });
+      } else {
+        $('.mobileHeader').stop().animate({
+          opacity: '0'
+        }, 200).css({
+          display: 'none'
+        });
+      }
+    });
+  } else {
+    $('.mobileHeader').css({
+      display: 'none'
+    });
+    $(window).scroll(function () {
+
+      $('.mobileHeader').css({
+        display: 'none'
+      });
+    });
+
   }
 });
 
+
+
 $(document).ready(function () {
+  if ($(window).width() < 1024) {
+    $(window).scroll(function () {
+      var scroll = $(document).scrollTop();
+
+      if (scroll == 0) {
+        $('.mobileHeader').stop().animate({
+          opacity: '1'
+        }, 200).css({
+          display: 'block'
+        });
+      } else {
+        $('.mobileHeader').stop().animate({
+          opacity: '0'
+        }, 200).css({
+          display: 'none'
+        });
+      }
+    });
+  } else {
+    $(window).scroll(function () {
+
+      $('.mobileHeader').css({
+        display: 'none'
+      });
+    });
+
+  }
+
+
+
   $(".mobileHeader .hamburger").click(function () {
 
     $(this).toggleClass('active');
