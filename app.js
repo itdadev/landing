9a3803723736e88fc9d15081bfc021f1de6334b9
lilly0305 <yhklilly0305@gmail.com>
2022-01-28 11:03:40 +0900

    app.js 경로 수정 완료

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "./index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
