95d2b4f0bd217be497155f83c466b7620c87a047
lilly0305 <yhklilly0305@gmail.com>
2022-01-28 13:45:55 +0900

    Revert "퇴근 전 commit 메일 오류 수정중"
    This reverts commit 935a2c91c63a081376e418deb06bde263628620d.

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ app.use(bodyParser.json())
 
 app.use(express.static(__dirname + './public'));
 
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
@@ -48,7 +47,7 @@ const upload = multer({
 }).single('image');
 
 
-app.post("/requst-form.html", (req, res) => {
+app.post("/public/requst-form.html", (req, res) => {
   // execute this middleware to upload the image
   upload(req, res, function (err) {
     if (err) {
@@ -112,9 +111,15 @@ app.post("/requst-form.html", (req, res) => {
 });
 
 
+<<<<<<< HEAD
 app.post("/contact.html", (req, res) => {
   upload(req, res, function (err) {
     if (err) {
+=======
+app.post("/public/contact.html", (req, res) => {
+  upload(req, res, function(err){
+    if(err){
+>>>>>>> parent of 935a2c9 (퇴근 전 commit 메일 오류 수정중)
       console.log(err)
       return res.end("Something went wrong!")
     } else {
