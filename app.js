86075ef32270c5bee8362421e7af3ac97fdd239c
lilly0305 <yhklilly0305@gmail.com>
2022-02-03 18:56:26 +0900

    Access-Control-Allow-Headers 설정

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
-  // res.header('Access-Control-Allow-Headers', 'application/json'); 
+  res.header('Access-Control-Allow-Headers', 'application/json'); 
   res.sendFile(__dirname + "/public/index.html");
 });
 
