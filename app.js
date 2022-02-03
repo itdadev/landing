1e8042ede4fdb97b522fd4719aa80ef0fce9bcb3
lilly0305 <yhklilly0305@gmail.com>
2022-02-03 14:28:54 +0900

    header에 access-control-allow-headers 넣어주기

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+res.header('Access-Control-Allow-Headers', 'application/json'); 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
