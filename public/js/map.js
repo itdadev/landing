c3e666b884f9e9bfae496a8c0232949d0dd28d93
lilly0305 <yhklilly0305@gmail.com>
2022-02-04 14:22:45 +0900

    cutomizing map

--- public/js/map.js
+++ public/js/map.js
@@ -3,7 +3,7 @@ var HOME_PATH = window.HOME_PATH || '.';
 var itda = new naver.maps.LatLng(37.499617546951285, 127.02959009953081),
   map = new naver.maps.Map('map', {
     center: itda.destinationPoint(0, 500),
-    zoom: 15
+    zoom: 10
   }),
   marker = new naver.maps.Marker({
     map: map,
@@ -17,7 +17,7 @@ var itda = new naver.maps.LatLng(37.499617546951285, 127.02959009953081),
   });
 
 var contentString = [
-  '<a href="https://www.itdadev.com" target="_blank" style="display:block; border-radius: 8px; padding: 20px; border:1px solid #5452F6; overflow: hidden; background: #ffffff;">',
+  '<a href="https://www.itdadev.com" target="_blank" style="display:block; border-radius: 8px; padding: 20px; border:1px solid #5452F6; overflow: hidden; background: #ffffff; font-size: 14px;">',
     '<div class="iw_inner" style="display: flex; align-items:center; justify-content:center;">',
       '<img src="img/logo.png" width="80" height="auto" alt="잇다" class="thumb" style="margin-right: 10px;"/>',
       '<span style="font-weight: bold; color: #595959;">서울특별시 강남구 강남대로<br />94길 28 (강남역 12번 출구 도보 4분)</span>',
