920a37249ea7c5e0968d48177daa6838667f75f2
lilly0305 <yhklilly0305@gmail.com>
2022-02-04 14:21:26 +0900

    infowindow customizing #2

--- public/js/map.js
+++ public/js/map.js
@@ -10,7 +10,7 @@ var itda = new naver.maps.LatLng(37.499617546951285, 127.02959009953081),
     position: itda,
     icon: {
       url: 'https://www.itdadev.com/public/img/anchor.svg',
-      size: new naver.maps.Size(10, 20),
+      size: new naver.maps.Size(22, 35),
       origin: new naver.maps.Point(0, 0),
       anchor: new naver.maps.Point(11, 35)
     }
@@ -19,8 +19,8 @@ var itda = new naver.maps.LatLng(37.499617546951285, 127.02959009953081),
 var contentString = [
   '<a href="https://www.itdadev.com" target="_blank" style="display:block; border-radius: 8px; padding: 20px; border:1px solid #5452F6; overflow: hidden; background: #ffffff;">',
     '<div class="iw_inner" style="display: flex; align-items:center; justify-content:center;">',
-      '<img src="img/logo.png" width="80" height="auto" alt="잇다" class="thumb" />',
-      '<span style="font-weight: bold; color: #595959;">서울 강남구 강남대로<br />94길 28 (강남역 12번 출구 도보 4분)</span>',
+      '<img src="img/logo.png" width="80" height="auto" alt="잇다" class="thumb" style="margin-right: 10px;"/>',
+      '<span style="font-weight: bold; color: #595959;">서울특별시 강남구 강남대로<br />94길 28 (강남역 12번 출구 도보 4분)</span>',
     '</div>',
   ' </a>',
 ].join('');
